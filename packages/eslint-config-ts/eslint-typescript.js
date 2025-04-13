65df361acbd8dbc4883571c8041da084286ca89d
alex8088 <244096523@qq.com>
2025-04-13 18:42:16 +0800

    fix(eslint-config-ts): remove deprecated rules

--- packages/eslint-config-ts/eslint-typescript.js
+++ packages/eslint-config-ts/eslint-typescript.js
@@ -8,10 +8,8 @@ module.exports = [
       '@typescript-eslint/explicit-function-return-type': 'error',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
-      '@typescript-eslint/no-empty-interface': ['error', { allowSingleExtends: true }],
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/no-var-requires': 'off'
+      '@typescript-eslint/no-non-null-assertion': 'off'
     }
   },
   {
