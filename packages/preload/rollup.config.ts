5220c192df2f3b35a2e86e4f9a3705e926567ed7
alex8088 <244096523@qq.com>
2025-04-19 22:39:23 +0800

    refactor: use rollup instead of unbuild

---
+++ packages/preload/rollup.config.ts
@@ -0,0 +1,38 @@
+import { createRequire } from 'node:module'
+import { defineConfig } from 'rollup'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import ts from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import rm from 'rollup-plugin-rm'
+
+const require = createRequire(import.meta.url)
+const pkg = require('./package.json')
+
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    external: ['electron'],
+    plugins: [
+      rm('dist', 'buildStart'),
+      resolve(),
+      commonjs(),
+      ts({
+        compilerOptions: {
+          rootDir: 'src',
+          declaration: true,
+          declarationDir: 'dist/types'
+        }
+      })
+    ]
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: pkg.types, format: 'es' }],
+    plugins: [dts(), rm('dist/types', 'buildEnd')]
+  }
+])
